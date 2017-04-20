ae7a1f707dba0605595178193c8afb968fdf6e9e
al0mie <alex.mokrencko@gmail.com>
2017-04-20 23:06:07 +0300

    different repositories for database drivers

---
+++ src/Repositories/MyslqTranslatorRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Vsch\TranslationManager\Repositories;
+
+use Vsch\TranslationManager\Models\Translation;
+use Vsch\TranslationManager\Repositories\Interfaces\ITranslatorRepository;
+
+class MysqlTranslatorRepository extends TranslatorRepository implements ITranslatorRepository
+{
+
+}
---
+++ src/Repositories/PostresTranslatorRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Vsch\TranslationManager\Repositories;
+
+use Vsch\TranslationManager\Models\Translation;
+use Vsch\TranslationManager\Repositories\Interfaces\ITranslatorRepository;
+
+class PostgresTranslatorRepository extends TranslatorRepository implements ITranslatorRepository
+{
+    
+}
