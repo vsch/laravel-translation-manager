98d36c6f8684e4513a95a7522af6e92210146dfb
al0mie <alex.mokrencko@gmail.com>
2017-04-24 01:44:59 +0300

    move contract methods in repository interface

--- src/Repositories/Interfaces/ITranslatorRepository.php
+++ src/Repositories/Interfaces/ITranslatorRepository.php
@@ -4,5 +4,49 @@ namespace Vsch\TranslationManager\Repositories\Interfaces;
 
 interface ITranslatorRepository
 {
+    public function updateIsDeletedByIds($rowId);
 
+    public function setNotUsedForAllTranslations();
+
+    public function updateStatusForTranslations($status, $updated_at, $translationIds);
+
+    public function getInsertTranslationsElement($translation, $timeStamp);
+
+    public function deleteTranslationsForIds($translationIds);
+
+    public function updateValuesByStatus();
+
+    public function updateUsedTranslationsForGroup($keys, $group);
+
+    public function updateIsDeletedByGroupAndKey($group, $key, $value);
+
+    public function updateGroupKeyStatusById($group, $key, $id);
+
+    public function selectTranslationsByLocaleAndGroup($locale, $db_group);
+
+    public function selectSourceByGroupAndKey($group, $key);
+
+    public function insertTranslations($values);
+
+    public function deleteTranslationWhereIsDeleted($group = null);
+
+    public function deleteTranslationByGroup($group);
+
+    public function updateValueInGroup($group);
+
+    public function searchByRequest($q, $displayWhere);
+
+    public function allTranslations($group, $displayLocales);
+
+    public function stats($displayLocales);
+
+    public function findMismatches($displayLocales, $primaryLocale, $translatingLocale);
+
+    public function selectToDeleteTranslations($group, $key, $locale, $rowIds);
+
+    public function selectKeys($src, $dst, $userLocales, $srcgrp, $srckey, $dstkey, $dstgrp);
+
+    public function copyKeys($dstgrp, $dstkey, $rowId);
+
+    public function findFilledGroups();
 }
